7c0c1f4d5dc4bb327791cd3ef4a1a0cbc4f0e0dd
Jaime García <55400857+jaime-garj@users.noreply.github.com>
2024-10-26 21:58:15 +0200

    Added YouTube and Twitch channels.

--- app/src/main/java/com/gaarj/iptvplayer/data/services/ApiService.kt
+++ app/src/main/java/com/gaarj/iptvplayer/data/services/ApiService.kt
@@ -15,6 +15,7 @@ import java.io.InputStreamReader
 import java.io.OutputStream
 import java.net.HttpURLConnection
 import java.net.URL
+import java.util.Scanner
 
 class ApiService {
 
@@ -98,36 +99,34 @@ class ApiService {
                     url = streamSource.url
                     return url
                 }
-                val apiCalls = streamSource.apiCalls?.sortedBy { it.index }
+                val apiCalls = streamSource.apiCalls.sortedBy { it.index }
                 var data: String? = null
 
-                if (apiCalls != null) {
-                    for (apiCall in apiCalls) {
-                        url = apiCall.url
-                        Log.d("ApiService", "URL: $url")
-                        val index = apiCall.index
-                        if (data != null){
-                            url = url.replace("{${index - 1}}", data)
+                for (apiCall in apiCalls) {
+                    url = apiCall.url
+                    Log.d("ApiService", "URL: $url")
+                    val index = apiCall.index
+                    if (data != null){
+                        url = url.replace("{${index - 1}}", data)
+                    }
+                    val headers = apiCall.headers
+                    val headersMap = headers?.let { getHeadersMapFromApiCallHeadersObject(it) }
+
+                    val method = apiCall.method
+                    val body = apiCall.body
+
+                    val apiResponseKeys = apiCall.apiResponseKeys
+
+                    if (apiCall.type == "json"){
+                        val json = getJSONFromURL(url, method, headersMap, body)
+
+                        for (apiResponseKey in apiResponseKeys) {
+                            val jsonPath = apiResponseKey.jsonPath
+                            data = JsonPath.parse(json)?.read<String>(jsonPath)
                         }
-                        val headers = apiCall.headers
-                        val headersMap = headers?.let { getHeadersMapFromApiCallHeadersObject(it) }
-
-                        val method = apiCall.method
-                        val body = apiCall.body
-
-                        val apiResponseKeys = apiCall.apiResponseKeys
-
-                        if (apiCall.type == "json"){
-                            val json = getJSONFromURL(url, method, headersMap, body)
-
-                            for (apiResponseKey in apiResponseKeys) {
-                                val jsonPath = apiResponseKey.jsonPath
-                                data = JsonPath.parse(json)?.read<String>(jsonPath)
-                            }
-                        } else if (apiCall.type == "html") {
-                            url = getURLFromHTML(url, apiCall.stringSearch!!)
-                            return url
-                        }
+                    } else if (apiCall.type == "html") {
+                        url = getURLFromHTML(url, apiCall.stringSearch!!)
+                        return url
                     }
                 }
                 if (url != null) {
@@ -143,16 +142,90 @@ class ApiService {
 
             }
             else if (streamSource.streamSourceType == StreamSourceTypeItem.TWITCH) {
-                val clientId = "kimne78kx3ncx6brgo4mv6wki5h1ko"
+                val clientId = "ue6666qo983tsx6so1t0vnawi233wa"
+                val gqlUrl = "https://gql.twitch.tv/gql#origin=twilight"
+                val userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
                 val headers = mapOf(
-                    "Client-Id" to clientId)
-                val data = getJSONFromURL("https://gql.twitch.tv/gql", method = "POST", headers = headers, body = "{\"operationName\":\"PlaybackAccessToken_Template\",\"query\":\"query PlaybackAccessToken_Template(\$login: String!, \$isLive: Boolean!, \$vodID: ID!, \$isVod: Boolean!, \$playerType: String!, \$platform: String!) {  streamPlaybackAccessToken(channelName: \$login, params: {platform: \$platform, playerBackend: \\\"mediaplayer\\\", playerType: \$playerType}) @include(if: \$isLive) {    value    signature   authorization { isForbidden forbiddenReasonCode }   __typename  }  videoPlaybackAccessToken(id: \$vodID, params: {platform: \$platform, playerBackend: \\\"mediaplayer\\\", playerType: \$playerType}) @include(if: \$isVod) {    value    signature   __typename  }}\",\"variables\":{\"isLive\":true,\"login\":\"" + streamSource.url + "\",\"isVod\":false,\"vodID\":\"\",\"playerType\":\"site\",\"platform\":\"web\"}}")
+                    "Client-Id" to clientId,
+                    "User-Agent" to userAgent)
+                val gql = "[\n" +
+                        "   {\n" +
+                        "      \"operationName\":\"StreamMetadata\",\n" +
+                        "      \"query\":\"query StreamMetadata(\$channelLogin: String!) { user(login: \$channelLogin) { id primaryColorHex isPartner profileImageURL(width: 70) primaryTeam { id name displayName } squadStream { id members { id } status } channel { id chanlets { id } } lastBroadcast { id title } stream { id type createdAt game { id name } } } }\",\n" +
+                        "      \"variables\":{\n" +
+                        "         \"channelLogin\":\"" + streamSource.url + "\"\n" +
+                        "      },\n" +
+                        "      \"extensions\":{\n" +
+                        "         \"persistedQuery\":{\n" +
+                        "            \"version\":1,\n" +
+                        "            \"sha256Hash\":\"a647c2a13599e5991e175155f798ca7f1ecddde73f7f341f39009c14dbf59962\"\n" +
+                        "         }\n" +
+                        "      }\n" +
+                        "   },\n" +
+                        "   {\n" +
+                        "      \"query\":\"query UseViewCount(\$channelLogin: String!) { user(login: \$channelLogin) { id stream { id viewersCount } } }\",\n" +
+                        "      \"operationName\":\"UseViewCount\",\n" +
+                        "      \"variables\":{\n" +
+                        "         \"channelLogin\":\"" + streamSource.url + "\"\n" +
+                        "      },\n" +
+                        "      \"extensions\":{\n" +
+                        "         \"persistedQuery\":{\n" +
+                        "            \"sha256Hash\":\"00b11c9c428f79ae228f30080a06ffd8226a1f068d6f52fbc057cbde66e994c2\",\n" +
+                        "            \"version\":1\n" +
+                        "         }\n" +
+                        "      }\n" +
+                        "   },\n" +
+                        "   {\n" +
+                        "      \"extensions\":{\n" +
+                        "         \"persistedQuery\":{\n" +
+                        "            \"sha256Hash\":\"639d5f11bfb8bf3053b424d9ef650d04c4ebb7d94711d644afb08fe9a0fad5d9\",\n" +
+                        "            \"version\":1\n" +
+                        "         }\n" +
+                        "      },\n" +
+                        "      \"query\":\"query UseLive(\$channelLogin: String!) { user(login: \$channelLogin) { id login stream { id createdAt } }\",\n" +
+                        "      \"operationName\":\"UseLive\",\n" +
+                        "      \"variables\":{\n" +
+                        "         \"channelLogin\":\"" + streamSource.url + "\"\n" +
+                        "      }\n" +
+                        "   },\n" +
+                        "   {\n" +
+                        "      \"extensions\":{\n" +
+                        "         \"persistedQuery\":{\n" +
+                        "            \"sha256Hash\":\"0828119ded1c13477966434e15800ff57ddacf13ba1911c129dc2200705b0712\",\n" +
+                        "            \"version\":1\n" +
+                        "         }\n" +
+                        "      },\n" +
+                        "      \"operationName\":\"PlaybackAccessToken\",\n" +
+                        "      \"variables\":{\n" +
+                        "         \"isLive\":true,\n" +
+                        "         \"isVod\":false,\n" +
+                        "         \"login\":\"" + streamSource.url + "\",\n" +
+                        "         \"playerType\":\"frontpage\",\n" +
+                        "         \"vodID\":\"\"\n" +
+                        "      },\n" +
+                        "      \"query\":\"query PlaybackAccessToken(\$login: String! \$isLive: Boolean! \$vodID: ID! \$isVod: Boolean! \$playerType: String!) { streamPlaybackAccessToken(channelName: \$login params: {platform: \\\"web\\\" playerBackend: \\\"mediaplayer\\\" playerType: \$playerType}) @include(if: \$isLive) { value signature } videoPlaybackAccessToken(id: \$vodID params: {platform: \\\"web\\\" playerBackend: \\\"mediaplayer\\\" playerType: \$playerType}) @include(if: \$isVod) { value signature } }\"\n" +
+                        "   }\n" +
+                        "]".trimIndent()
+
+                val data = getJSONFromURL(gqlUrl, method = "POST", headers = headers, body = gql)
 
                 val id = streamSource.url
-                val accessTokenValue = JsonPath.parse(data)?.read<String>("data.streamPlaybackAccessToken.value")
-                val accessTokenSignature = JsonPath.parse(data)?.read<String>("data.streamPlaybackAccessToken.signature")
+                val accessTokenValue = JsonPath.parse(data)?.read<String>("[3].data.streamPlaybackAccessToken.value")
+                val accessTokenSignature = JsonPath.parse(data)?.read<String>("[3].data.streamPlaybackAccessToken.signature")
 
-                url ="https://usher.ttvnw.net/api/channel/hls/${id}.m3u8?client_id=${clientId}&token=${accessTokenValue}&sig=${accessTokenSignature}&allow_source=true&allow_audio_only=true"
+                url = "https://usher.ttvnw.net/api/channel/hls/${id}.m3u8?acmb=e30=&allow_source=true&fast_bread=true&p=&play_session_id=&player_backend=mediaplayer&playlist_include_framerate=true&reassignments_supported=true&sig=${accessTokenSignature}&supported_codecs=avc1&token=${accessTokenValue}&transcode_mode=vbr_v1&cdm=wv&player_version=1.20.0"
+                //url ="https://usher.ttvnw.net/api/channel/hls/${id}.m3u8?client_id=${clientId}&token=${accessTokenValue}&sig=${accessTokenSignature}&allow_source=true&allow_audio_only=true"
+            }
+            else if (streamSource.streamSourceType == StreamSourceTypeItem.YOUTUBE) {
+                val html = fetchHtmlFromUrl("https://www.youtube.com/@${streamSource.url}/live")
+
+                val regex = """"hlsManifestUrl":"(https://[^"]+\.m3u8)"""".toRegex()
+                val matchResult = regex.find(html)
+
+                url = matchResult?.groups?.get(1)?.value
+            }
+            else {
+                url = streamSource.url
             }
 
             return url
@@ -172,6 +245,19 @@ class ApiService {
                 headersMap[header.key] = header.value
             }
             return headersMap
+        }
+
+        private fun fetchHtmlFromUrl(urlString: String): String {
+            val url = URL(urlString)
+            val connection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "GET"
+
+            val inputStream = connection.inputStream
+            val scanner = Scanner(inputStream).useDelimiter("\\A")
+            val htmlContent = if (scanner.hasNext()) scanner.next() else ""
+
+            connection.disconnect()
+            return htmlContent
         }
     }
 
