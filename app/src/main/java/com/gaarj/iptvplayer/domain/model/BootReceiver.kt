e2f528fb32ada6c8c239ea5c36538b3b56d6260e
Jaime García <55400857+jaime-garj@users.noreply.github.com>
2025-04-01 15:11:07 +0200

    Disable auto boot on system start

--- app/src/main/java/com/gaarj/iptvplayer/domain/model/BootReceiver.kt
+++ app/src/main/java/com/gaarj/iptvplayer/domain/model/BootReceiver.kt
@@ -3,15 +3,15 @@ package com.gaarj.iptvplayer.domain.model
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.gaarj.iptvplayer.ui.view.HomeActivity
+import com.gaarj.iptvplayer.ui.view.PlayerActivity
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+        /*if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
             // Start your app's main activity
-            val i = Intent(context, HomeActivity::class.java)
+            val i = Intent(context, PlayerActivity::class.java)
             //i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             //context.startActivity(i)
-        }
+        }*/
     }
 }
