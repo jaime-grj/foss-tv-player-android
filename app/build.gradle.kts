648237e3f955e09f36168eefb69394a71f52d19a
jaime-garj <55400857+jaime-garj@users.noreply.github.com>
2025-04-04 02:48:45 +0200

    Change package name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,11 +8,11 @@ plugins {
 }
 
 android {
-    namespace = "com.gaarj.iptvplayer"
+    namespace = "com.gaarx.iptvplayer"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.gaarj.iptvplayer"
+        applicationId = "com.gaarx.iptvplayer"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
