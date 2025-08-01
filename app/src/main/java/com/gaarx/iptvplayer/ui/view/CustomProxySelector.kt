dc0bbb5616760d9ba22b3e8c13496ec4cfc535e7
Jaime García <55400857+jaime-grj@users.noreply.github.com>
2025-08-01 20:51:13 +0200

    Separate CustomProxySelector

---
+++ app/src/main/java/com/gaarx/iptvplayer/ui/view/CustomProxySelector.kt
@@ -0,0 +1,18 @@
+package com.gaarx.iptvplayer.ui.view
+
+import android.util.Log
+import java.net.InetSocketAddress
+import java.net.Proxy
+import java.net.ProxySelector
+import java.net.URI
+
+
+class CustomProxySelector(private val proxyHost: String, private val proxyPort: Int) : ProxySelector() {
+    override fun select(uri: URI?): List<Proxy> {
+        return listOf(Proxy(Proxy.Type.HTTP, InetSocketAddress(proxyHost, proxyPort)))
+    }
+
+    override fun connectFailed(uri: URI?, sa: java.net.SocketAddress?, ioe: java.io.IOException?) {
+        Log.e("ProxySelector", "Connection failed: $uri", ioe)
+    }
+}
